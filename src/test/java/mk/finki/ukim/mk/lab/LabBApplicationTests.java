ac08015b11b6c147e0de64bad75173c86ba6c944
Filip <jakimov.filip15820@gmail.com>
2024-09-20 16:47:31 +0200

    Fixed naming in the test class

--- src/test/java/mk/finki/ukim/mk/lab/LabBApplicationTests.java
+++ src/test/java/mk/finki/ukim/mk/lab/LabBApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LabBApplicationTests {
+class LabGroupBApplication {
 
     @Test
     void contextLoads() {
